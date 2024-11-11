0aaba7dd531bc749a7ed8dc21922b3f8a9e8b4c2
LuckyX2-Stup <boyahseru@gmail.com>
2024-11-11 20:29:06 +0700

    datacamp

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'c23f420a-ad6f-477e-bf60-5193aae69dae';
 
-const พร็อกซีไอพีs = ['168.138.165.174'];
+const พร็อกซีไอพีs = ['195.114.193.161'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
